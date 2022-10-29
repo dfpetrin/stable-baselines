ba9ffe444b9c4b87100f03897a14cff94074fd92
Derrik Petrin <dfpetrin@cs.washington.edu>
2022-10-28 22:50:26 -0700

    feat(ppo2): add effective ordering

--- stable_baselines/ppo2/ppo2.py
+++ stable_baselines/ppo2/ppo2.py
@@ -471,6 +471,7 @@ class Runner(AbstractEnvRunner):
         # mb stands for minibatch
         mb_obs, mb_rewards, mb_actions, mb_values, mb_dones, mb_neglogpacs = [], [], [], [], [], []
         mb_states = self.states
+        mb_effective_steps, _ = np.indices((self.n_steps, self.n_envs))
         ep_infos = []
         for _ in range(self.n_steps):
             actions, values, self.states, neglogpacs = self.model.step(self.obs, self.states, self.dones)  # pytype: disable=attribute-error
@@ -512,15 +513,25 @@ class Runner(AbstractEnvRunner):
         mb_advs = np.zeros_like(mb_rewards)
         true_reward = np.copy(mb_rewards)
         last_gae_lam = 0
-        for step in reversed(range(self.n_steps)):
-            if step == self.n_steps - 1:
+        # compute decision and effect indexing
+        effective_sort = np.argsort(mb_effective_steps, axis=0)
+        # list of (effective_step_idx, decision_steps_arr)
+        steps = list(enumerate(effective_sort))
+        for effective_step, decision_steps in reversed(steps):
+            if effective_step == self.n_steps - 1:
                 nextnonterminal = 1.0 - self.dones
                 nextvalues = last_values
             else:
-                nextnonterminal = 1.0 - mb_dones[step + 1]
-                nextvalues = mb_values[step + 1]
-            delta = mb_rewards[step] + self.gamma * nextvalues * nextnonterminal - mb_values[step]
-            mb_advs[step] = last_gae_lam = delta + self.gamma * self.lam * nextnonterminal * last_gae_lam
+                nextnonterminal = 1.0 - mb_dones[effective_step + 1]
+                _, next_decision_steps = steps[effective_step + 1]
+                nextvalues = next_decision_steps.choose(mb_values)
+            delta = (
+                mb_rewards[effective_step]
+                + self.gamma * nextvalues * nextnonterminal
+                - decision_steps.choose(mb_values)
+            )
+            last_gae_lam = delta + self.gamma * self.lam * nextnonterminal * last_gae_lam
+            mb_advs[decision_steps, np.arange(self.n_envs)] = last_gae_lam
         mb_returns = mb_advs + mb_values
 
         mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, true_reward = \
